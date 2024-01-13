3e0fcd371edc95034e111afc170bb856fecbc981
Siddhant <siddhantmishrabusy949@gmail.com>
2024-01-13 22:22:16 +0530

    added gooogle adsense in head tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,30 +1,56 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/navbar/Navbar'
-import Footer from '@/components/footer/Footer'
-import Provider from '@/SessionProvider'
-import { ThemeContextProvider } from '@/context/ThemeContext'
-import ThemeProvider from '@/providers/ThemeProvider'
+import Head from 'next/head';
+import './globals.css';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/navbar/Navbar';
+import Footer from '@/components/footer/Footer';
+import Provider from '@/SessionProvider';
+import { ThemeContextProvider } from '@/context/ThemeContext';
+import ThemeProvider from '@/providers/ThemeProvider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Devscribe',
   description: 'Cut the carp off',
-}
+  googleAdsenseAccount: 'ca-pub-4583277760904170', 
+  googleAnalyticsId: 'G-FT7QFYKVW4',
+};
 
 export default function RootLayout({ children, session }) {
   return (
-  
     <html lang="en">
       <body className={inter.className}>
+        <Head>
+          {/* Google AdSense script */}
+          <script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js`}></script>
+          <script>
+            {`
+              (adsbygoogle = window.adsbygoogle || []).push({
+                google_ad_client: "${metadata.googleAdsenseAccount}",
+                enable_page_level_ads: true
+              });
+            `}
+          </script>
+
+          {/* Google Analytics script */}
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=${metadata.googleAnalyticsId}`}></script>
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag() {
+                window.dataLayer.push(arguments);
+              }
+              gtag('js', new Date());
+              gtag('config', '${metadata.googleAnalyticsId}');
+            `}
+          </script>
+        </Head>
         <ThemeContextProvider>
           <ThemeProvider>
             <Provider>
-          
               <div className="container">
                 <div className="wrapper">
-                <Navbar />
+                  <Navbar />
                   {children}
                   <Footer />
                 </div>
@@ -34,6 +60,5 @@ export default function RootLayout({ children, session }) {
         </ThemeContextProvider>
       </body>
     </html>
-
-  )
+  );
 }
