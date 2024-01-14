075acce7dc6ad61f737b722952ddf14c7bd283b5
Siddhant <siddhantmishrabusy949@gmail.com>
2024-01-14 15:18:56 +0530

    added analytics file

--- src/app/_app.js
+++ src/app/_app.js
@@ -1,46 +1,42 @@
-import Head from "next/head";
-import { useRouter } from "next/router";
-import Script from "next/script";
-import { useEffect } from "react";
-import * as gtag from "../lib/gtag"
+import "@/styles/globals.css"
+import Script from "next/script"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import * as gtag from "gtag"
 
-const App = ({ Component, pageProps }) => {
-  const router = useRouter();
+export default function App({ Component, pageProps }) {
+  const router = useRouter()
   useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
+    const handleRouteChange = url => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplete", handleRouteChange)
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
-      <Head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', '${gtag.GA_TRACKING_ID}', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-      </Head>
-      {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
         strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
-      <Component {...pageProps} />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />;
     </>
-  );
-};
-
-export default App;
+  )
+}
